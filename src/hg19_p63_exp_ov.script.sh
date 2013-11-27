fbbaafee89322d942069f1331ad5c42a809fbbba
J Falck <jfalck@lilo3.science.ru.nl>
2013-11-27 11:47:24 +0100

    added some results

--- src/hg19_p63_exp_ov.script.sh
+++ src/hg19_p63_exp_ov.script.sh
@@ -11,9 +11,9 @@ mkdir -p ../results/tables/p63_exp_vs_ovs_overlap;
 #done;
 
 # parameter j determines the number of cores used.
-/home/jfalck/bin/parallel -j20 --gnu --keep-order --eta 'bedtools shuffle -chrom -seed {1} -i ../data/hg19/p63all_sort_merge_hg.19.bed -g ../data/hg19/hg19.genome | bedtools intersect -a stdin -b ../data/hg19/encode/peaks/{2} | wc -l | xargs echo exp  >> ../results/tables/p63_exp_vs_ovs_overlap/hg19_p63_obs_vs_exp_{2}' ::: $(seq 1 1000) ::: $(ls ../data/hg19/encode/peaks/);
+parallel -j10 --gnu --keep-order --eta 'bedtools shuffle -chrom -seed {1} -i ../data/hg19/p63all_sort_merge_hg.19.bed -g ../data/hg19/hg19.genome | bedtools intersect -a stdin -b ../data/hg19/encode/peaks/{2} | wc -l | xargs echo exp {1} >> ../results/tables/p63_exp_vs_ovs_overlap/hg19_p63_obs_vs_exp_{2}' ::: $(seq 1 1000) ::: $(ls ../data/hg19/encode/peaks/);
 
 # append the observed overlap to the file
 for i in $(ls ../data/hg19/encode/peaks/);
-  do bedtools intersect -a ../data/hg19/p63all_sort_merge_hg.19.bed -b ../data/hg19/encode/peaks/$i | wc -l |xargs echo obs >> ../results/tables/p63_exp_vs_ovs_overlap/hg19_p63_obs_vs_exp_$i
+  do bedtools intersect -a ../data/hg19/p63all_sort_merge_hg.19.bed -b ../data/hg19/encode/peaks/$i | wc -l |xargs echo obs 1001 >> ../results/tables/p63_exp_vs_ovs_overlap/hg19_p63_obs_vs_exp_$i
 done;
