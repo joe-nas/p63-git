9c19a657adec7d748177e7185cee046cb5626e44
joe-nas <jonas.falck@student.ru.nl>
2013-11-28 15:49:47 +0100

    ...

--- src/venn_plot.R
+++ src/venn_plot.R
@@ -1,7 +1,18 @@
 require("Vennerable")
+setwd(".")
+
+# shortcut to folder and filenames
 histoneFolder<- "../results/tables/venn_diagrams/h3k4me1_h3k4me3_h3k27ac/"
 histoneFiles<-list.files("../results/tables/venn_diagrams/h3k4me1_h3k4me3_h3k27ac/",pattern="intersect")
-histoneNames<-sapply(X=histoneFiles,FUN=function(x){gsub(x,pattern="intersect_union_wgEncodeBroadChipSeqPeaks",replacement="")})
-print(histoneNames)[2,]
 histoneFiles<- paste(histoneFolder,histoneFiles,sep="")
+# loading data
 myData<-lapply(histoneFiles,function(x) read.table(x,header=F))
+# changing names
+m<-regexpr("*NhekH3k{1,2}[0-9]{1,2}[a-z]{1,2}[a-z]{1,2}[0-9]*",histoneFiles)
+histoneNames<-regmatches(histoneFiles,m)
+names(myData)<-histoneNames
+# compute and plot venn diagram
+myVenn<-Venn(list(myData$NhekH3k27ac[,1],
+                  myData$NhekH3k4me1[,1],
+                  myData$NhekH3k4me3[,1]),
+             SetNames=names(myData))
